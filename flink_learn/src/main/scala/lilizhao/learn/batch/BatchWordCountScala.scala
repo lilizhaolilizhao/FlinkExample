6892664d5d18907ec0ae88307b0473dc9f8a938c
lilizhao <lilizhaolilizhao@163.com>
2020-04-03 10:39:23 +0800

    模块调整

--- flink_learn/src/main/scala/lilizhao/learn/batch/BatchWordCountScala.scala
+++ flink_learn/src/main/scala/lilizhao/learn/batch/BatchWordCountScala.scala
@@ -4,8 +4,8 @@ import org.apache.flink.api.scala.ExecutionEnvironment
 
 object BatchWordCountScala {
   def main(args: Array[String]): Unit = {
-    val inputPath = "/Users/lilizhao/my_source/FlinkExample/src/main/scala/xuwei/tech/streaming/SocketWindowWordCountScala.scala"
-    val outPath = "/Users/lilizhao/my_source/FlinkExample/target/out.csv"
+    val inputPath = "/Users/lilizhao/my_source/LearningDemo/flink_learn/src/main/scala/lilizhao/learn/batch/BatchWordCountScala.scala"
+    val outPath = "/Users/lilizhao/my_source/LearningDemo/target/out.csv"
 
     val env = ExecutionEnvironment.getExecutionEnvironment
     val text = env.readTextFile(inputPath, "utf-8")
